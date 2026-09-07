be240dd0568f1998279547f4e4646620a726c505
KazeFX <1071525+KazeFX@users.noreply.github.com>
2026-09-07 21:54:07 +0200

    Almost done with cli

--- Lektion2/todo.cs
+++ Lektion2/todo.cs
@@ -1,40 +1,77 @@
-using System.Net.Mime;
-using System.Xml;
 
 public class todo
 {
-
     public static void Main(string[] args) {
         string sourcePath = "Todo_list.txt";
 
-        if (!File.Exists(sourcePath)) {
-            File.Create("Todo_list.md").Close();
+        if (!File.Exists(sourcePath)) 
+        {
+            File.Create("Todo_list.txt").Close();
         }
-        
 
         switch (args[0])
         {
             case "add":
-                string input;
-                File.AppendAllText(sourcePath, args[1]);
-                if (args[2] != null && args[2] == "--due")
+                if (args.Length < 2)
                 {
-                    File.AppendAllText(sourcePath, $"  tills {args[3]}");
+                    Console.WriteLine("No such command.");
+                    break;
                 }
-                File.AppendAllLines(sourcePath, ["\n"]);
-            break;
+
+                File.AppendAllLines(sourcePath, [args[1]]);
+                break;
 
             case "done":
+                if (args.Length < 2)
+                {
+                    Console.WriteLine("No task with that number.");
+                    break;
+                }
+
                 string[] todoList = File.ReadAllLines(sourcePath);
                 int taskCompleted = int.Parse(args[1]);
-                todoList[taskCompleted] = $"~~{todoList[taskCompleted]}~~";
-                File.AppendAllLines(sourcePath, todoList);
+
+                if (taskCompleted < 1 || taskCompleted >= todoList.Length)
+                {
+                    Console.WriteLine("No task with that number.");
+                    break;
+                }
+
+                int index = taskCompleted-1;
+                todoList[index] = $"DONE - {todoList[index]}";
+                File.WriteAllLines(sourcePath, todoList);
                 break;
 
             case "remove":
-                string[] todoList2 = File.ReadAllLines(sourcePath);
+                if (args.Length < 2)
+                {
+                    Console.WriteLine("No such task to remove.");
+                    break;
+                }
+
+                string[] todoListForRemoval = File.ReadAllLines(sourcePath);
                 int removeTask = int.Parse(args[1]);
-                todoList2[removeTask] = "";
+
+                if (removeTask < 1 || removeTask > todoListForRemoval.Length)
+                {
+                    Console.WriteLine("No such task to remove.");
+                    break;
+                }
+
+                int indexToRemove = removeTask-1; 
+                todoListForRemoval[indexToRemove] = "";
+                string[] newList = new string[todoListForRemoval.Length-1];
+
+                int j = 0;
+                for (int i = 0; i < todoListForRemoval.Length;i++)
+                {
+                    if (!todoListForRemoval[i].IsWhiteSpace())
+                    {
+                        newList[j] = todoListForRemoval[i];
+                    }
+                }
+
+                File.WriteAllLines(sourcePath, newList);
                 break;
         }
     }
