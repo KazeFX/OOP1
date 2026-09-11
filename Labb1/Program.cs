8ad26954760b3f4759d43464b180797ba48e0f4b
KazeFX <1071525+KazeFX@users.noreply.github.com>
2026-09-11 17:10:02 +0200

    try

--- Labb1/Program.cs
+++ Labb1/Program.cs
@@ -1,7 +1,17 @@
-﻿class Program
+﻿using System.Runtime.InteropServices;
+
+class Program
 {
-
-
+    static decimal baseTariff = 49.0m;
+    static bool membership = false;
+    static bool insurance = false;
+    static string sender = "";
+    static int weight = 0;
+    static decimal sum = 0.0m;
+    static decimal value = 0.0m;
+    static decimal weightTariff = 0.0m;
+    static decimal heavyGoodsSurcharge = 0.0m;
+    static decimal insuranceCost = 0.0m;
 
 
     public static void Main()
@@ -23,103 +33,120 @@
             switch (input)
             {
                 case "1":
-                    bool membership = false;
-                    bool insurance = false;
-
-                    Console.Write("Input weight: ");
-                    int inputWeight = int.Parse(Console.ReadLine());
-
-                    Console.Write("Are you a member?(yes/no): ");
-                    string memberPick = Console.ReadLine();
-                    if (memberPick.Equals("yes", StringComparison.CurrentCultureIgnoreCase))
-                    {
-                        membership = true;
-                    }
-
-                    Console.Write("Do you want to insure the package? yes/no: ");
-                    string pick = Console.ReadLine();
-                    if (pick.Equals("yes", StringComparison.OrdinalIgnoreCase))
-                    {
-                        insurance = true;
-                    }
-
-                    Console.WriteLine(calculateShippingPerPackage(inputWeight, membership, insurance));
+                    userPrompt();
+                    Console.WriteLine(calculateShippingPerPackage());
+                    printReceipt();
+                    resetFields();
                     break;
 
                 case "2":
-
                     break;
 
                 case "3":
                     Environment.Exit(0);
                     break;
-
-
             }
         }
     }
 
-    // public void userInfo()
-    // {
-    //     Console.Write("Are you a member?(yes/no): ");
-    //     string memberPick = Console.ReadLine();
-    //     if (memberPick.Equals("yes", StringComparison.CurrentCultureIgnoreCase))
-    //     {
-    //         membership = true;
-    //     }
-
-    //     Console.Write("Do you want to insure the package? yes/no: ");
-    //     string pick = Console.ReadLine();
-    //     if (pick.Equals("yes", StringComparison.OrdinalIgnoreCase))
-    //     {
-    //         insurance = true;
-    //     }
-
-    // }
-    static decimal calculateShippingPerPackage(int weight, bool membership, bool insurance)
+    static void userPrompt()
     {
-        decimal sum = 49.0m;
+        Console.Write("Input sender: ");
+        sender = Console.ReadLine();
+
+        Console.Write("Input weight: ");
+        weight = int.Parse(Console.ReadLine());
+
+        Console.Write("Input value: ");
+        value = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Are you a member?(yes/no): ");
+        string memberPick = Console.ReadLine();
+        if (memberPick.Equals("yes", StringComparison.CurrentCultureIgnoreCase))
+        {
+            membership = true;
+        }
+
+        Console.Write("Do you want to insure the package? yes/no: ");
+        string pick = Console.ReadLine();
+        if (pick.Equals("yes", StringComparison.OrdinalIgnoreCase))
+        {
+            insurance = true;
+        }
+    }
+
+    static void printReceipt()
+    {
+        Console.WriteLine("FRAKTVITTO");
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine($"Avsändare: {sender}");
+        Console.WriteLine($"Vikt: {weight}");
+        Console.WriteLine($"Innehållets värd: {value}");
+        Console.WriteLine($"Medlem: {membership}");
+        Console.WriteLine($"Försäkring: {insurance}");
+        Console.WriteLine();
+        Console.WriteLine($"Grundavgift: \t {baseTariff,-10}");
+        Console.WriteLine($"Viktavgift: \t {weightTariff,-10}");
+        Console.WriteLine($"Tunggodstillägg: \t {heavyGoodsSurcharge,-10}");
+        Console.WriteLine($"Försäkringsavgift: \t {insuranceCost,-10}");
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine($"Totalt att betala: \t {sum}");
+    }
+
+    static decimal calculateShippingPerPackage()
+    {
+        sum = baseTariff;
 
         if (membership)
         {
-            if (weight <= 5 && weight >= 0)
+            if (weight > 5 && weight <= 20)
             {
-                sum = sum;
-
+                weightTariff += (weight - 5) * 10m;
+                sum += weightTariff;
             }
             else if (weight > 20)
             {
-                sum += (weight - 20) * 40;
+                weightTariff = 150m;
+                heavyGoodsSurcharge = (weight - 20) * 30m;
+                weightTariff += heavyGoodsSurcharge;
+                sum += weightTariff;
             }
-            else if (weight <= 20 && weight > 5)
+            else
             {
-                sum += (weight - 5) * 10;
+                if (weight > 5 && weight <= 20)
+                {
+                    weightTariff += (weight - 2) * 10m;
+                    sum += weightTariff;
+                }
+                else if (weight > 20)
+                {
+                    weightTariff = 180m;
+                    heavyGoodsSurcharge = (weight - 20) * 30m;
+                    weightTariff += heavyGoodsSurcharge;
+                    sum += weightTariff;
+                }
             }
-
-        }
-
-        else
-        {
-            if (weight <= 2 && weight >= 0)
-            {
-                sum = sum;
-
-            }
-            else if (weight > 20)
-            {
-                sum += (weight - 20) * 40;
-            }
-            else if (weight <= 20 && weight > 5)
-            {
-                sum += (weight - 2) * 10;
-            }
-
         }
 
         if (insurance)
         {
-            sum *= 1.01m;
+            insuranceCost = value * 0.01m;
+            sum += insuranceCost;
         }
+
         return sum;
+    }
+
+    static void resetFields()
+    {
+        membership = false;
+        insurance = false;
+        sender = "";
+        weight = 0;
+        sum = 0.0m;
+        value = 0.0m;
+        weightTariff = 0.0m;
+        heavyGoodsSurcharge = 0.0m;
+        insuranceCost = 0.0m;
     }
 }
